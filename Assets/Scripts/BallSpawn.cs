44401e75cf0a6d4ffb7767fe7103036bd9d35aa7
Shashank Sinha <shashanksinha590@gmail.com>
2025-06-21 12:32:51 +0530

    Spawn script updated

--- Assets/Scripts/BallSpawn.cs
+++ Assets/Scripts/BallSpawn.cs
@@ -3,14 +3,10 @@ using UnityEngine;
 public class BallSpawn : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public GameObject ball;
+    public Transform Position;
     void Start()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        Instantiate(ball, Position.position, Quaternion.identity);
     }
 }
