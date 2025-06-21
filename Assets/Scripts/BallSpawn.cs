7bdf32d363e681efb6c17a1c630aa66c7dd66165
Shashank Sinha <shashanksinha590@gmail.com>
2025-06-21 15:38:04 +0530

    update

--- Assets/Scripts/BallSpawn.cs
+++ Assets/Scripts/BallSpawn.cs
@@ -3,12 +3,12 @@ using UnityEngine;
 public class BallSpawn : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    public GameObject ball;
+    public GameObject Ball;
     public Transform Position;
     public CameraFollow follow;
     void Start()
     {
-        GameObject go= Instantiate(ball, Position.position, Quaternion.identity);
-        follow.ball = ball.transform;
+        GameObject go= Instantiate(Ball, Position.position, Quaternion.identity);
+        follow.ball = Ball.transform;
     }
 }
