0f5c7b01d72d02f78402a5d72ece4988cb0a4252
Shashank Sinha <shashanksinha590@gmail.com>
2025-06-21 14:30:56 +0530

    Update CameraFollow.cs

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -3,14 +3,11 @@ using UnityEngine;
 public class CameraFollow : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
-
+    public Vector3 distance;
+    Transform ball;
     // Update is called once per frame
     void Update()
     {
-        
+        transform.position = ball.position + distance;
     }
 }
