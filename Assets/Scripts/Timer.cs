7298fc7425c651d4f7a46350d10de3a6d4dcac7a
Shashank Sinha <shashanksinha590@gmail.com>
2025-06-22 19:50:32 +0530

    Timer script test

---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public float time = 300;
+    Text remaining;
+    void Start()
+    {
+        remaining.text = "Time: " + time;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (time > 0)
+        {
+
+        }
+    }
+}
