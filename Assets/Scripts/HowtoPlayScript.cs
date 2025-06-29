1b9c8822722545cbf4b451f477390830886f2f1e
Shashank Sinha <shashanksinha590@gmail.com>
2025-06-29 15:45:22 +0530

    Updates to UI

--- Assets/Scripts/HowtoPlayScript.cs
+++ Assets/Scripts/HowtoPlayScript.cs
@@ -1,7 +1,14 @@
 using UnityEngine;
-
 public class HowtoPlayScript : MonoBehaviour
 {
     public GameObject howtoplay;
-
+    public GameObject instructions;
+    [HideInInspector]public void InstructionManual()
+    {
+        instructions.SetActive(true);
+    }
+    [HideInInspector]public void CloseInstructionManual()
+    {
+        instructions.SetActive(false);
+    }
 }
